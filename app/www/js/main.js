cabad7dd9771df1572bba931e3c90e499aa47697
thestuckster <stephenstucy@aol.com>
2014-06-01 13:02:41 -0500

    added toCanvas function under Global Functions

--- app/www/js/main.js
+++ app/www/js/main.js
@@ -66,6 +66,7 @@ var photo = function(){
 
 }
 
+
 //Global Vars
 var loco = new loco();
 	loco.get();
@@ -73,3 +74,13 @@ var loco = new loco();
 var photo = new photo();
 
 //Global Functions
+function toCanvas(base64){
+	var canvas = document.getElementById('imgCanvas');
+	var ctx    = canvas.getContext('2d');
+	var img    = new Image();
+	img.src    = "data:image/ jpeg;base64," + base64;
+	img.onload = function(){
+		ctx.drawImage(img, 0, 0);
+	}
+};
+	
